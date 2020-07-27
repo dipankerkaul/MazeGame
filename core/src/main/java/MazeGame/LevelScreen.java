557442221f078c7ef7126cba9e3bb3f65babd3f3
Jane Doe <42276172+dipankerkaul@users.noreply.github.com>
2020-07-27 18:34:54 +0530

    Third Testable Commit!

--- core/src/main/java/MazeGame/LevelScreen.java
+++ core/src/main/java/MazeGame/LevelScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Input.Keys;
 public class LevelScreen extends BaseScreen
 {
     Maze maze;
+    Hero hero;
 
     public void initialize()
     {
@@ -14,9 +15,17 @@ public class LevelScreen extends BaseScreen
         background.setColor(Color.GRAY);
         background.setSize(768, 700);
         maze = new Maze(mainStage);
+        hero = new Hero(0,0,mainStage);
+        hero.centerAtActor( maze.getRoom(0,0) );
     }
     public void update(float dt)
-    { }
+    {
+        for (BaseActor wall : BaseActor.getList(mainStage, Wall.class))
+        {
+            hero.preventOverlap(wall);
+        }
+
+    }
     public boolean keyDown(int keyCode)
     {
         if ( keyCode == Keys.R )
